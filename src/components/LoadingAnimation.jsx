50a19d3458f0053c39771313e800aae8457a4d66
Androz2091 <androz2091@gmail.com>
2021-04-15 12:25:21 +0200

    :sparkles: Support darkmode for loading animation

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './LoadingAnimation.css';
+import { useStoreState } from 'easy-peasy';
 
-const LoadingAnimation = ({ size }) => (
-    <div className="ball-pulse smallBall text-center">
-        <div style={{ width: size, height: size }} />
-        <div style={{ width: size, height: size }} />
-        <div style={{ width: size, height: size }} />
-    </div>
-);
+const LoadingAnimation = ({ size }) => {
+
+    const darkmode = useStoreState((state) => state.darkmode);
+
+    return (
+        <div className="ball-pulse smallBall text-center">
+            <div style={{ width: size, height: size, backgroundColor: !darkmode ? 'white' : 'black' }} />
+            <div style={{ width: size, height: size, backgroundColor: !darkmode ? 'white' : 'black' }} />
+            <div style={{ width: size, height: size, backgroundColor: !darkmode ? 'white' : 'black' }} />
+        </div>
+    )
+};
 
 LoadingAnimation.defaultProps = {
     size: '15px'
